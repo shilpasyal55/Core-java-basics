e1cb69e6bb491e183c0ef02ef0be1d6ab081e67e
shilpa syal <shilpasyal55@gmail.com>
2019-04-06 12:38:54 +0530

    Control flow statements

---
+++ 9ControlFlow.java
@@ -0,0 +1,50 @@
+//IF STATEMENT
+
+class IfStatement {
+
+	public void executeIf(int counter) {
+		if(counter >= 50) { 
+	    	System.out.print("Counter is greater than or equal to 50.");
+		} 
+		else if (counter > 30) { 
+		    System.out.print("Counter is greater than 30."); 
+		} 
+		else { 
+		   System.out.println("Counter is less than 30");
+		}
+	}
+}
+
+class SwitchStatement {
+	String color = "red";
+	public void executeSwitch() {
+		switch(color) {
+			case "red" :
+				System.out.println("Red Color");
+				break;
+			case "blue":
+				System.out.println("Blue Color");
+				break;
+			default:
+				System.out.println("Random Color");
+		}
+	}
+}
+
+class TernaryStatement {
+	int findMaximum(int a, int b){
+      //if a > b, it returns a, if not it returns b
+	    return (a > b) ? a : b;
+	}
+}
+
+public class ControlFlow {
+	public static void main(String[] args) {
+		IfStatement obj = new IfStatement();
+		obj.executeIf(10);
+		SwitchStatement obj1 = new SwitchStatement();
+		obj1.executeSwitch();
+		TernaryStatement obj2 = new TernaryStatement();
+		System.out.println(obj2.findMaximum(5,9));
+	}
+}
---
+++ Loops.java
@@ -0,0 +1,79 @@
+class ForLoop {
+	void executeForLoop() {
+		for (int i = 0; i < 10 ; i ++) {
+			System.out.println(i);
+		}
+	}
+}
+
+class ForEachLoop {
+	int[] marks = {25,78,56,12,10};
+
+	void executeForEachLoop() {
+		for (int mark : marks) {
+			System.out.println(mark);
+		}
+	}
+}
+
+class WhileLoop {
+	void executeWhileLoop() {
+		int x = 0; 
+		while(x < 10) { 
+		    System.out.println(x); //prints x until x < 10 evaluates to false
+			x++; 
+		}
+	}
+}
+
+class DoWhileLoop {
+	void executeDoWhileLoop() {
+		int x = 0; 
+			do { 
+			    System.out.println(x); 
+			    x++; 
+			} while(x < 10);
+	}
+}
+
+class BreakContinue {
+//break statement moves out of the loop 
+	 void executeBreak() {
+	 	for (int i = 0; i < 10; i++) {
+			  if (i == 4) {
+			    break;
+			  }
+			  System.out.println(i);
+		}
+	 }
+// The continue statement breaks one iteration (in the loop), 
+//if a specified condition occurs, and continues with the next iteration in the loop.
+
+
+	 void executeContinue() {
+	 	for (int i = 0; i < 10; i++) {
+		  if (i == 4) {
+		    continue;
+		  }
+		  System.out.println(i);
+		}
+	 }
+}
+
+
+
+
+public class Loops {
+	public static void main(String[] args) {
+		ForLoop obj = new ForLoop();
+		obj.executeForLoop();
+		ForEachLoop obj1 = new ForEachLoop();
+		obj1.executeForEachLoop();
+		WhileLoop obj2 = new WhileLoop();
+		obj2.executeWhileLoop();
+		DoWhileLoop obj3 = new DoWhileLoop();
+		obj3.executeDoWhileLoop();
+		BreakContinue  obj4 = new BreakContinue();
+		obj4.executeContinue();
+	}
+}
